bf3c4ec89bf73194b9dd96033d6bc73501d6ac17
laiyeowming <74872256+laiyeowming@users.noreply.github.com>
2021-07-03 03:13:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 """ .py for first telegram bot"""
-
 import telebot
 
 TOKEN = '1790716225:AAHSdpfgFMSX7dlCVgBuu_gZyNu-LYeo0ng'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['hello', 'help'])
-def send_welcome(message):
+def send_welcome(message): """msg for /hello and /help"""
     bot.reply_to(message, "Howdy, how are you doing?")
 
 bot.polling()
