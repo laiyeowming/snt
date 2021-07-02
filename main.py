d1c7d7eb5ab32a6ed093e063774856ed015be06d
laiyeowming <74872256+laiyeowming@users.noreply.github.com>
2021-07-03 02:56:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import telebot
+
 TOKEN = '1790716225:AAHSdpfgFMSX7dlCVgBuu_gZyNu-LYeo0ng'
 bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(commands=['hello', 'help'])
-def send_welcome(message): bot.reply_to(message, "Howdy, how are you doing?")
+def send_welcome(message): 
+  bot.reply_to(message, "Howdy, how are you doing?")
+
 bot.polling()
