aaf6fff923c78b37bc31829ee288be01eca9f880
laiyeowming <74872256+laiyeowming@users.noreply.github.com>
2021-07-03 00:17:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello")
+import telebot
+import os
+
+API_key = os.getenv('API_key')
+bot = telebot.TeleBot("API_key")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, 5how are you doing?")
+
+bot.polling()
