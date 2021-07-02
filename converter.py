2bedbb0d90dde8c977e824b5d84f15a407631d74
laiyeowming <74872256+laiyeowming@users.noreply.github.com>
2021-07-02 17:47:27 +0800

    .py added

---
+++ converter.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://alpha-vantage.p.rapidapi.com/query"
+
+querystring = {"to_currency":"JPY","function":"CURRENCY_EXCHANGE_RATE","from_currency":"USD"}
+
+headers = {
+    'x-rapidapi-key': "2a9cdbe220msh52b5f56e5b1a596p1dc6dfjsnecc135e4c219",
+    'x-rapidapi-host': "alpha-vantage.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
